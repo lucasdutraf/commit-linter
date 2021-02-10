3f8acb587ec89b62077563f84c1291613e815605
Lucas Dutra <ldutra98@gmail.com>
2021-02-10 18:27:18 -0300

    minor changes

--- commit_test.py
+++ commit_test.py
@@ -2,6 +2,6 @@ def say_hello(pronoun: str) -> str:
     greeting = f"Hello {pronoun}!"
     return greeting
 
-greeting = say_hello('bro')
+greeting = say_hello('brother')
 
 print(f"{greeting} how are u doin")
