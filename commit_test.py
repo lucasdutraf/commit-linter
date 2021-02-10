9c02e0c57b0b57e75c448da173766092bc16c848
Lucas Dutra <ldutra98@gmail.com>
2021-02-10 19:11:29 -0300

    fix: update variable value

--- commit_test.py
+++ commit_test.py
@@ -2,6 +2,6 @@ def say_hello(pronoun: str) -> str:
     greeting = f"Hello {pronoun}!"
     return greeting
 
-greeting = say_hello('brother')
+greeting = say_hello('cara')
 
 print(f"{greeting} how are u doin")
