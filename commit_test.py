744a35cf6a940ac833f10556cbb2dc3266abe2a3
Lucas Dutra <ldutra98@gmail.com>
2021-03-02 18:26:43 -0300

    ci: #2 fix typo

--- commit_test.py
+++ commit_test.py
@@ -2,6 +2,6 @@ def say_hello(pronoun: str) -> str:
     greeting = f"Hello {pronoun}!"
     return greeting
 
-greeting = say_hello('cara')
+greeting = say_hello('caa')
 
 print(f"{greeting} how are u doin")
